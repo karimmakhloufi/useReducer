29493a923e33acd5d896f2f13a0b067ce4ccd20c
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-09-23 13:25:53 +0200

    Delete with filter

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { useState } from "react";
 const App = () => {
   const [todos, setTodo] = useState([]);
   const [inputValue, setInput] = useState("");
+  const [id, setId] = useState(0);
   return (
     <>
       <input value={inputValue} onChange={(e) => setInput(e.target.value)} />
       <button
         onClick={() => {
-          setTodo([...todos, inputValue]);
+          setTodo([...todos, { id, message: inputValue }]);
           setInput("");
+          setId(id + 1);
         }}
       >
         add
@@ -17,8 +19,15 @@ const App = () => {
       <br />
       {todos.map((todo) => (
         <>
-          <span>{todo}</span>
-          <button>Delete</button>
+          <span>{todo.message}</span>
+          <span>{todo.id}</span>
+          <button
+            onClick={() => {
+              setTodo(todos.filter((el) => el.id !== todo.id));
+            }}
+          >
+            Delete
+          </button>
           <br />
         </>
       ))}
