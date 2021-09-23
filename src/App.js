4f9f872a99a7f17571c05c0dbf1e4fc0455b3099
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-09-23 13:14:34 +0200

    pre delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [todos, setTodo] = useState([]);
+  const [inputValue, setInput] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={inputValue} onChange={(e) => setInput(e.target.value)} />
+      <button
+        onClick={() => {
+          setTodo([...todos, inputValue]);
+          setInput("");
+        }}
+      >
+        add
+      </button>
+      <br />
+      {todos.map((todo) => (
+        <>
+          <span>{todo}</span>
+          <button>Delete</button>
+          <br />
+        </>
+      ))}
+    </>
   );
-}
+};
 
 export default App;
